8d6d6cc79d9b8c7c5a679b4bdb4151930fbfd3f0
John P Vajda <jvajda@newrelic.com>
2021-02-22 11:17:10 -0700

    added port 8000

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 //You'll need to run  node index.js and then just go to your
 //browser at the localhost url it spits out to get the agent to report data.
-// this app is associated with v2 user Real Basic User and v1 Basic user.
+// this app is associated with v2 FSO user account
 // Swap the license key on the newrelic.js file to go between the 2
+// use port 3000 for local testing
 
 const express = require("express");
 require("newrelic");
 const app = express();
-const port = 3000;
+// const port = 3000;
+const port = 8000;
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  // console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://${port}`);
 });
